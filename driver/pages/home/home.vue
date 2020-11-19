a4c04849c09e5d9ad304a22c09e882f4e4564297
long long A dog <qingshanyuluo@gmail.com>
2020-11-19 21:41:44 +0800

    添加司机 uniapp

---
+++ driver/pages/home/home.vue
@@ -0,0 +1,48 @@
+<template>
+	<view>
+		<view class="page-body">
+			<view class="page-section page-section-gap">
+				<map style="width: 100%; height: 300px;" :latitude="latitude" :longitude="longitude" :markers="covers">
+				</map>
+			</view>
+		</view>
+		<button @click="start()">开始记录</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				id: 0, // 使用 marker点击事件 需要填写id
+				title: 'map',
+				latitude: 38.909,
+				longitude: 116.39742,
+				covers: [{
+					latitude: this.latitude,
+					longitude: this.longitude,
+					iconPath: '../../static/location.png'
+				}]
+			}
+		},
+		methods: {
+			start: function(){
+				setInterval(function() {
+					uni.getLocation({
+					    type: 'gcj02',
+					    success: function (res) {
+					        console.log('当前位置的经度：' + res.longitude);
+							// this.longitude = res.longitude;
+					        console.log('当前位置的纬度：' + res.latitude);
+							// this.latitude = res.latitude;
+					    }
+					});
+				}, 10);
+			}
+		},
+	}
+</script>
+
+<style>
+
+</style>
